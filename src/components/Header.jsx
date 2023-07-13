a69959bf331a63b78a7c11ed2cde44c61d9fe36d
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-12 23:23:11 -0500

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div id="header">
+        <div className="name">Eduardo Alvarado</div>
+        <div className="title">Junior Web Developer</div>
+      </div>
+    );
+  }
+}
+
+export default Header;
