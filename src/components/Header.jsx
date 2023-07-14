6de0916e59f9455e9654bf70049e89f14bdc9ca2
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-13 20:14:04 -0500

    Styling edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,13 @@ class Header extends Component {
   render() {
     return (
       <div id="header">
-        <div className="name">Eduardo Alvarado</div>
-        <div className="title">Junior Web Developer</div>
+        <div>
+          <div className="name">
+            <span>Eduardo </span>
+            Alvarado
+          </div>
+          <div className="title">Junior Web Developer</div>
+        </div>
         <div id="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus magni odio nobis voluptatem nostrum eaque molestiae quis. Voluptatibus officia a molestiae est totam voluptate quis exercitationem facere, quo laudantium enim tenetur. Impedit facilis quas amet a delectus voluptates aliquam sint voluptatibus! Quaerat, beatae eaque delectus dignissimos cum quo voluptatibus? Unde.</div>
       </div>
     );
