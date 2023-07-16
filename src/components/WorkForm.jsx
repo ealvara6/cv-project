bb2a9724cbaf5fd82f2da02655aa83e65525ca63
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-16 15:06:46 -0500

    Add work form change

--- src/components/WorkForm.jsx
+++ src/components/WorkForm.jsx
@@ -3,28 +3,30 @@ import React, { Component } from 'react';
 class WorkForm extends Component {
   render() {
     const {
+      id,
       dates,
       position,
       company,
       tasks,
+      handleChange,
     } = { ...this.props };
     return (
       <form action="" className="work-form">
         <div className="dates">
           <label htmlFor="dates">Dates</label>
-          <input type="text" name="dates" id="dates" value={dates} />
+          <input type="text" name="dates" id="dates" value={dates} onChange={(e) => handleChange(id, 'dates', e)} />
         </div>
         <div className="position">
           <label htmlFor="position">Position</label>
-          <input type="text" name="position" id="position" value={position} />
+          <input type="text" name="position" id="position" value={position} onChange={(e) => handleChange(id, 'position', e)} />
         </div>
         <div className="company">
           <label htmlFor="company">Company</label>
-          <input type="text" name="company" id="company" value={company} />
+          <input type="text" name="company" id="company" value={company} onChange={(e) => handleChange(id, 'company', e)} />
         </div>
         <div className="tasks">
           <label htmlFor="tasks">Tasks</label>
-          <textarea name="tasks" id="tasks" cols="30" rows="10" defaultValue={tasks} />
+          <textarea name="tasks" id="tasks" cols="30" rows="10" value={tasks} onChange={(e) => handleChange(id, 'tasks', e)} />
         </div>
       </form>
     );
